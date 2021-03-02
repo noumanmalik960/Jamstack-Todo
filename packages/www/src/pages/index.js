be29ca31e4760a31c078bea0dbacafbea3244fab
noumanmalik960 <noumanmalik960@gmail.com>
2021-03-02 13:56:33 +0500

    added netlify identity widget

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, Heading, Button, Flex } from 'theme-ui'
+import netlifyIdentity from 'netlify-identity-widget'
+
 const Index = () => {
+
+  useEffect(() => {
+    netlifyIdentity.init({})
+  })
   return (
     <Container>
       <Flex sx={{ flexDirection: 'column' }}>
         <Heading as="h1" sx={{ p: 10 }}>Add your Todo to track your tasks!</Heading>
-        <Button onClick={() => alert("clicked")}>Login</Button>
+        <Button onClick={() => netlifyIdentity.open()}>Login</Button>
       </Flex>
     </Container>
   )
