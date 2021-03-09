6754174a8082bd88603c230a43b4bdcf1e8f9304
noumanmalik960 <noumanmalik960@gmail.com>
2021-03-10 02:29:37 +0500

    added step6

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,21 +1,36 @@
-import React, { useEffect } from 'react'
-import { Container, Heading, Button, Flex } from 'theme-ui'
-import netlifyIdentity from 'netlify-identity-widget'
+import React, { useContext } from "react";
+import { Container, Heading, Button, Flex, NavLink } from "theme-ui";
+import { Link } from "gatsby";
+import { IdentityContext } from "../../identity-context";
+export default props => {
+  const { user, identity: netlifyIdentity } = useContext(IdentityContext);
 
-const Index = () => {
-
-  useEffect(() => {
-    netlifyIdentity.init({})
-  })
   return (
     <Container>
-      <Flex sx={{ flexDirection: 'column' }}>
-        <Heading as="h1" sx={{ p: 10 }}>Add your Todo to track your tasks!</Heading>
-        <Button onClick={() => netlifyIdentity.open()}>Login</Button><br />
-        <Button onClick={() => console.log(netlifyIdentity.currentUser())}>Log current user</Button>
+      <Flex as="nav">
+        <NavLink as={Link} to="/" p={2}>
+          Home
+        </NavLink>
+        <NavLink as={Link} to={"/app"} p={2}>
+          Dashboard
+        </NavLink>
+        {user && (
+          <NavLink href="#!" p={2}>
+            {user.user_metadata.full_name}
+          </NavLink>
+        )}
+      </Flex>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as="h1">Get Stuff Done</Heading>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            netlifyIdentity.open();
+          }}
+        >
+          Log In
+        </Button>
       </Flex>
     </Container>
-  )
-}
-
-export default Index
+  );
+};
